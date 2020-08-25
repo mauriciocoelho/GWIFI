74afcdcc0a8988288f978b1d39659bf7a90d131e
Mauricio Rodrigues Coelho <mauricio.coelho@itpacporto.local.br>
2020-08-25 12:51:00 -0300

    alteração db

--- config/database_visitante.php
+++ config/database_visitante.php
@@ -1,8 +1,8 @@
 <?php
-	$username="root";
-	$password="";
-	$host="localhost";
-	$database="172.19.0.153";
+	$username="radius";
+	$password="radpass";
+	$host="172.19.0.153";
+	$database="radcheck";
 	$db_link=mysqli_connect($host,$username,$password,$database)or die("Erro na conexao com o Banco de Dados".mysqli_error($db_link));
 	if (mysqli_connect_error()){
 	echo "Não foi possível conectar-se ao MySql. Por favor, tente novamente";
--- config/database_visitantes.php
+++ config/database_visitantes.php
@@ -2,7 +2,7 @@
 	$username="radius";
 	$password="radpass";
 	$host="172.19.0.153";
-	$database="radius";
+	$database="radcheck";
 	$db_link=mysqli_connect($host,$username,$password,$database)or die("Erro na conexao com o Banco de Dados".mysqli_error($db_link));
 	if (mysqli_connect_error()){
 	echo "Não foi possível conectar-se ao MySql. Por favor, tente novamente";
